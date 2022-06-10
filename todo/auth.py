5253f9d3450b6cb1a45331acb52f61e4c8a4f55f
Cesar Espinosa <cesar_iesch@hotmail.com>
2022-06-09 22:11:34 -0500

    Todo terminado

--- todo/auth.py
+++ todo/auth.py
@@ -58,6 +58,36 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            return redirect(url_for('todo.index'))
+
         flash(error)
+
     return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        db, c = get_db()
+        c.execute(
+            'SELECT * FROM user WHERE id = %s', (user_id, )
+        )
+        g.user = c.fetchone()
+    
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('auth.login'))
